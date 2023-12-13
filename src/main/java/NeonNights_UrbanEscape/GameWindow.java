5e26d28f5804630f047e22e58178223a01e36a51
g2jkj0274 <g2jkj0274@gmail.com>
2023-12-13 19:48:57 +0900

    When you press the start button, you will be moved to another panel.

--- src/main/java/NeonNights_UrbanEscape/GameWindow.java
+++ src/main/java/NeonNights_UrbanEscape/GameWindow.java
@@ -1,41 +1,74 @@
-package NeonNights_UrbanEscape; // 패키지 선언, 실제 패키지 명에 맞게 수정해야 할 수도 있습니다.
+package NeonNights_UrbanEscape;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameWindow extends JFrame {
-
-    private ImageIcon backgroundImage;
+    // CardLayout과 JPanel 선언
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
 
     public GameWindow() {
-        setTitle("Neon Nights Urban Escape"); // 게임 창의 제목 설정
-        setSize(1280, 720); // 게임 창의 크기 설정
-        setDefaultCloseOperation(EXIT_ON_CLOSE); // 닫기 버튼을 누르면 프로그램 종료
-        setLocationRelativeTo(null); // 창을 화면 가운데에 위치
-        setResizable(false); // 창 크기 변경 불가능하게 설정
+        setTitle("Neon Nights Urban Escape");
+        setSize(1280, 720);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
 
-        // 이미지 로드 및 크기 조정
+        // CardLayout 초기화
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        // 배경 이미지 로드 및 설정
         ImageIcon originalIcon = new ImageIcon(getClass().getResource("/images/main.png"));
         Image image = originalIcon.getImage();
-        Image resizedImage = image.getScaledInstance(1280, 720,  Image.SCALE_SMOOTH);
+        Image resizedImage = image.getScaledInstance(1280, 720, Image.SCALE_SMOOTH);
         ImageIcon resizedIcon = new ImageIcon(resizedImage);
-
-        // 배경 이미지를 가진 레이블 생성
         JLabel backgroundLabel = new JLabel(resizedIcon);
-        backgroundLabel.setLayout(new FlowLayout()); // 레이아웃 설정
 
-        // CustomButton 생성
+        // 메인 메뉴 패널 설정
+        JPanel mainMenuPanel = new JPanel(new BorderLayout());
+        mainMenuPanel.add(backgroundLabel, BorderLayout.CENTER);
+        cardPanel.add(mainMenuPanel, "MainMenu");
+
+        // 테스트 게임 화면 패널 설정
+        JPanel testGamePanel = new TestGamePanel(); // TestGamePanel은 여러분의 게임 콘텐츠를 포함할 클래스입니다.
+        cardPanel.add(testGamePanel, "TestGame");
+
+        // 시작 버튼 생성 및 액션 리스너 추가
         CustomButton startButton = new CustomButton("Start Game");
-        CustomButton optionsButton = new CustomButton("Options");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cardLayout.show(cardPanel, "TestGame");
+            }
+        });
 
-        // 버튼을 배경 레이블에 추가
-        backgroundLabel.add(startButton);
-        backgroundLabel.add(optionsButton);
+        // 옵션 버튼 생성 및 설정 (선택적)
+        CustomButton optionsButton = new CustomButton("Options");
+        // 옵션 버튼에 대한 액션 리스너 추가 (선택적)
+
+        // 메인 메뉴 패널에 버튼 추가
+        JPanel buttonPanel = new JPanel(); // 버튼을 위한 새로운 패널
+        buttonPanel.setOpaque(false); // 패널 배경을 투명하게 설정
+        buttonPanel.add(startButton);
+        buttonPanel.add(optionsButton);
+        mainMenuPanel.add(buttonPanel, BorderLayout.SOUTH); // 버튼 패널을 남쪽에 배치
 
         // 컨텐트 팬 설정
-        setContentPane(backgroundLabel);
+        setContentPane(cardPanel);
 
         // 창 표시
         setVisible(true);
+    }
+
+    // 테스트 게임 화면을 위한 패널 클래스
+    private class TestGamePanel extends JPanel {
+        public TestGamePanel() {
+            setBackground(Color.BLACK); // 배경 색 설정
+            // 여기에 게임 컨텐츠를 구성
+        }
     }
 }
